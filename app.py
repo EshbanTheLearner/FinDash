1ce31bc2fe4968bd0e61e8cd10ed456551f6e73f
Eshban Suleman <eshban9492@gmail.com>
2023-02-09 19:29:09 +0500

    added scaffolding to load data

--- app.py
+++ app.py
@@ -2,15 +2,32 @@ import streamlit as st
 import yfinance as yf
 import mplfinance as mpf
 import pandas as pd
+import datetime
+import time
 
 st.set_page_config(page_title="FinDash", layout="wide", page_icon="💸")
 
-ticks = pd.read_csv("data/ticks.csv", header=None)
-ticks = sorted(ticks.iloc[:][0].tolist())
+ticks = pd.read_csv("data/ticks.csv")
+ticks = sorted(ticks.iloc[:]["Symbol"].tolist())
+
+def load_data(ticker, start_date, end_date):
+    time.sleep(5)
+    st.write(f"Let's view **{ticker}** from **{start_date}** to **{end_date}**")
 
 with st.sidebar:
-    st.subheader("Select a Stock")
-    ticker = st.selectbox("Select a stock", ticks)
+    st.subheader("FinDash 💸")
+    ticker = st.selectbox("Select a Stock", ticks)
+    col1, col2 = st.columns(2)
+    with col1:
+        start_date = st.date_input("From", datetime.date(2023, 1, 1))
+    with col2:
+        end_date = st.date_input("To", datetime.datetime.today())
+
+    load_status = st.button(f"Analyze {ticker} Data")
 
 st.header("FinDash 💸")
-st.write(f"You selected: {ticker}")
+#st.write(f"Let's view **{ticker}** from **{start_date}** to **{end_date}**")
+
+if load_status:
+    with st.spinner(f"Loading {ticker} Data"):
+        load_data(ticker, start_date, end_date)
