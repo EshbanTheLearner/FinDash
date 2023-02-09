634b961666af7205c8113157b8dfedaf57d881bb
Eshban Suleman <eshban9492@gmail.com>
2023-02-09 18:50:46 +0500

    Added sample ticks data

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
-import streamlit as sts
+import streamlit as st
 import yfinance as yf
 import mplfinance as mpf
+import pandas as pd
+
+st.set_page_config(page_title="FinDash", layout="wide", page_icon="💸")
+
+ticks = pd.read_csv("data/ticks.csv", header=None)
+ticks = sorted(ticks.iloc[:][0].tolist())
+
+with st.sidebar:
+    st.subheader("Select a Stock")
+    ticker = st.selectbox("Select a stock", ticks)
+
+st.header("FinDash 💸")
+st.write(f"You selected: {ticker}")
